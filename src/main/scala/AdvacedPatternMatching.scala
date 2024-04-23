1b0032f1439f2c1d9191842fc3837175be120433
YuvrajSoni-Ksolves <yuvraj.soni@ksolves.com>
2024-04-23 13:48:57 +0530

    advanced pattern matching part 2

--- src/main/scala/AdvacedPatternMatching.scala
+++ src/main/scala/AdvacedPatternMatching.scala
@@ -41,15 +41,15 @@ object AdvacedPatternMatching {
     //      case _ => "No property"
     //    }
 
-    object singleDigit{
-      def unapply(arg : Int ): Option[Boolean] = {
-        if(arg > -10 && arg < 10)Some(true)
+    object singleDigit {
+      def unapply(arg: Int): Option[Boolean] = {
+        if (arg > -10 && arg < 10) Some(true)
         else None
       }
     }
-    object even{
-      def unapply(arg : Int) : Option[Boolean] ={
-        if(arg %2==0 && arg < 20) Some(true)
+    object even {
+      def unapply(arg: Int): Option[Boolean] = {
+        if (arg % 2 == 0 && arg < 20) Some(true)
         else None
       }
     }
@@ -61,5 +61,51 @@ object AdvacedPatternMatching {
     }
 
     println(matchProperty)
+    // infix patterns
+    case class Or[A, B](a: A, b: B)
+    val either = Or(2, "two")
+    val humanDescription = either match {
+      case number Or string => s"$number is written as $string"
+    }
+    println(humanDescription)
+
+    //decomposing sequences
+    abstract class MyList[+A] {
+      def head: A = ???
+
+      def tail: MyList[A] = ???
+    }
+    case object Empty extends MyList[Nothing]
+    case class Cons[+A](override val head: A, override val tail: MyList[A]) extends MyList[A]
+    object MyList {
+      def unapplySeq[A](list: MyList[A]): Option[Seq[A]] =
+        if (list == Empty) Some(Seq.empty)
+        else unapplySeq(list.tail).map(list.head +: _)
+    }
+    val myList: MyList[Int] = Cons(1, Cons(2, Cons(3, Empty)))
+    val decomposed = myList match {
+      case MyList(1, 2, _*) => "starting with 1 and 2"
+      case _ => "something else"
+    }
+    println(decomposed)
+
+    //custom return type for unapply
+    // isEmpty -> Boolean
+    // get -> Something
+
+    abstract class Wrapper[T] {
+      def isEmpty: Boolean
+      def get: T
+    }
+    object PersonWrapper {
+      def unapply(person: Person): Wrapper[String] = new Wrapper[String] {
+        def isEmpty: Boolean = false
+        def get: String = person.name
+      }
+    }
+    println(bob match
+      case PersonWrapper(n) => s"This person's name is ${n}"
+      case _ => "any other case "
+    )
   }
 }
